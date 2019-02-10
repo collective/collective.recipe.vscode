53eed7e27a378a4e8fae14505c2dc47d2f15a844
Md Nazrul Islam <connect2nazrul@gmail.com>
2019-02-10 18:21:44 +0100

    fix classifiers in setup py

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     # Get more from https://pypi.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Environment :: Web Environment",
-        "Development Status ::  4 - Beta",
+        "Development Status :: 4 - Beta",
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.0",
