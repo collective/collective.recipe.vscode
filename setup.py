a2c75715ed336eaf630fb8f3acc1cf1b3864bcd7
Md Nazrul Islam <connect2nazrul@gmail.com>
2020-06-26 21:40:43 +0600

    keep python 2.7 in ignore list.

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
